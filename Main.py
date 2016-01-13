74502b5b807cd20af717c676ca0b4518b6d5bcf3
sarahjamieson <sarah.jamieson@postgrad.manchester.ac.uk>
2016-01-13 16:57:10 +0000

    Added Docstring.

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import warnings
 from ExcelToSQL import ExcelToSQL
-
 warnings.simplefilter("ignore", UserWarning)
+
+"""Module takes an excel file and database as inputs and runs the ExcelToSQL class to add excel files to a database."""
 
 excel_file = 'HSP_REEP1.xlsx'
 db = 'Primer_v1.db'
